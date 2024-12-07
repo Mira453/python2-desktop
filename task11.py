300d34e48542bee1bd2c806cac8bc6de0bbe5ae2
Mira453 <miroslavabous3@gmail.com>
2024-12-07 16:29:27 +0200

    comit4

---
+++ task11.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+matrix = np.random.randint(1, 100, (5, 5))
+print(matrix)
+
+max_in_rows = np.max(matrix, axis=1)
+print("Найбільші елементи в кожному рядку: ", max_in_rows)
+
+min_in_rows = np.min(matrix, axis=1)
+print("Найменший елемент в кожному рядку: ", min_in_rows)
